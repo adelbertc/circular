7fa45d483ca4ef68ba6c4ada6d071bbc7b55b916
Adelbert Chang <adelbertc@gmail.com>
2016-08-28 22:06:58 -0700

    Add PIsos combinators and instances for stdlib, fixes #6

--- core/src/main/scala/circular/PIso.scala
+++ core/src/main/scala/circular/PIso.scala
@@ -16,10 +16,14 @@
 
 package circular
 
-import cats.arrow.Category
+import cats.arrow.{Category, Split}
+import cats.Eq
 import cats.implicits._
+import scala.annotation.tailrec
 
 final case class PIso[A, B](to: A => Option[B], from: B => Option[A]) {
+  import PIso.{associate, cons, id, iterateDriver, nil, unit}
+
   def lift[F[_]: PInvariant](fa: F[A]): F[B] =
     PInvariant[F].pimap(fa)(this)
 
@@ -28,15 +32,47 @@ final case class PIso[A, B](to: A => Option[B], from: B => Option[A]) {
   def compose[C](other: PIso[C, A]): PIso[C, B] = PIso(c => other.to(c).flatMap(to), b => from(b).flatMap(other.from))
 
   def andThen[C](other: PIso[B, C]): PIso[A, C] = other.compose(this)
+
+  def split[C, D](other: PIso[C, D]): PIso[(A, C), (B, D)] =
+    PIso({ case (a, c) => to(a).product(other.to(c)) },
+         { case (b, d) => from(b).product(other.from(d)) })
+
+  def first[C]: PIso[(A, C), (B, C)] = split(PIso.id[C])
+
+  def second[C]: PIso[(C, A), (C, B)] = PIso.id[C].split(this)
+
+  def iterate(implicit ev: A =:= B): PIso[A, A] = {
+    val paa = this.asInstanceOf[PIso[A, A]]
+    PIso(a => Some(iterateDriver(paa.to, a)),
+         a => Some(iterateDriver(paa.from, a)))
+  }
+
+  def foldLeft(implicit ev: A <:< (A, B)): PIso[(A, List[B]), A] = {
+    val paba = this.asInstanceOf[PIso[(A, B), A]]
+    val step: PIso[(A, List[B]), (A, List[B])] =
+      id[A].split(cons[B].invert) andThen
+      associate[A, B, List[B]]    andThen
+      paba.split(id[List[B]])
+
+    step.iterate.andThen(id[A].split(nil[B].invert)).andThen(unit[A].invert)
+  }
 }
 
-object PIso extends PIsoInstances with PIsoFunctions
+object PIso extends PIsoInstances with PIsoFunctions {
+  @tailrec
+  private def iterateDriver[A](step: A => Option[A], state: A): A = step(state) match {
+    case None    => state
+    case Some(a) => iterateDriver(step, a)
+  }
+}
 
 private[circular] sealed abstract class PIsoInstances {
-  implicit val circularCategoryForPIso: Category[PIso] = new Category[PIso] {
+  implicit val circularInstancesForPIso: Category[PIso] with Split[PIso] = new Category[PIso] with Split[PIso] {
     def compose[A, B, C](f: PIso[B, C], g: PIso[A, B]): PIso[A, C] = f.compose(g)
 
     def id[A]: PIso[A, A] = PIso(Some(_), Some(_))
+
+    def split[A, B, C, D](f: PIso[A, B], g: PIso[C, D]): PIso[(A, C), (B, D)] = f.split(g)
   }
 }
 
@@ -47,4 +83,69 @@ trait PIsoFunctions {
     PIso(a => Some(to(a)), b => Some(from(b)))
 
   def fromPrism[A, B](to: A => Option[B], from: B => A): PIso[A, B] = PIso(to, b => Some(from(b)))
+
+  // PIsos for standard library - List, Vector, Option, Either
+
+  def nil[A]: PIso[Unit, List[A]] = {
+    def nilOnly(list: List[A]): Option[Unit] = list match {
+      case Nil    => Some(())
+      case _ :: _ => None
+    }
+    PIso(_ => Some(Nil), nilOnly)
+  }
+
+  def cons[A]: PIso[(A, List[A]), List[A]] = {
+    def consOnly(list: List[A]): Option[(A, List[A])] = list match {
+      case Nil     => None
+      case a :: as => Some((a, as))
+    }
+    PIso( { case (a, as) => Some(a :: as) }, consOnly)
+  }
+
+  def vnil[A]: PIso[Unit, Vector[A]] = {
+    def nilOnly(vector: Vector[A]): Option[Unit] = vector match {
+      case Vector() => Some(())
+      case _ +: _   => None
+    }
+    PIso(_ => Some(Vector.empty[A]), nilOnly)
+  }
+
+  def vcons[A]: PIso[(A, Vector[A]), Vector[A]] = {
+    def consOnly(vector: Vector[A]): Option[(A, Vector[A])] = vector match {
+      case Vector() => None
+      case a +: as  => Some((a, as))
+    }
+    PIso( { case (a, as) => Some(a +: as) }, consOnly)
+  }
+
+  def none[A]: PIso[Unit, Option[A]] = {
+    def noneOnly(opt: Option[A]): Option[Unit] = opt match {
+      case None    => Some(())
+      case Some(_) => None
+    }
+    PIso(_ => Some(None), noneOnly)
+  }
+
+  def some[A]: PIso[A, Option[A]] = PIso(a => Some(Some(a)), identity)
+
+  def left[A, B]: PIso[A, Either[A, B]] = PIso(a => Some(Left(a)), _.left.toOption)
+
+  def right[A, B]: PIso[B, Either[A, B]] = PIso(b => Some(Right(b)), _.right.toOption)
+
+  def associate[A, B, C]: PIso[(A, (B, C)), ((A, B), C)] =
+    PIso.fromIso({ case (a, (b, c)) => ((a, b), c) },
+                 { case ((a, b), c) => (a, (b, c)) })
+
+  def commute[A, B]: PIso[(A, B), (B, A)] =
+    PIso.fromIso({ case (a, b) => (b, a) }, { case (b, a) => (a, b) })
+
+  def unit[A]: PIso[A, (A, Unit)] = PIso.fromIso((_, ()), _._1)
+
+  def element[A](a: A)(implicit A: Eq[A]): PIso[Unit, A] =
+    PIso(_ => Some(a), a2 => if (A.eqv(a, a2)) Some(()) else None)
+
+  def subset[A](p: A => Boolean): PIso[A, A] = {
+    def f(a: A): Option[A] = if (p(a)) Some(a) else None
+    PIso(f, f)
+  }
 }
